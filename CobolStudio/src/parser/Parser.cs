36d5a4ac4b80c92c252ea416d57cb8dc2c48ce14
Peter Fisk <peter.fisk@gmail.com>
2025-11-21 13:31:15 -0500

    parser

---
+++ CobolStudio/src/parser/Parser.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CobolStudio.src.parser
+{
+    internal class Parser
+    {
+        static Parser instance = null;
+        public static Parser GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Parser();
+            }
+            return instance;
+        }
+
+        public static string ParseSource(string source)
+        {
+            // Placeholder for actual COBOL parsing logic
+            return $"Parsed COBOL source of length {source.Length}";
+        }
+    }
+
+}
